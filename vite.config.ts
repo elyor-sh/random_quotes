f9754e7af016fc755bdb519b4aef0e2121e282e8
Elyor Shodiyorov <elyor.sh.99@mail.ru>
2022-12-24 03:28:47 +0500

    initial commit

---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import * as path from "path";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 3000,
+    host: '127.0.0.1'
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
+})
